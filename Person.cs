0f8f1662bb56032a26a8635e318d74ea4e2610e3
R3ap4r <R3ap4r@telenet.be>
2021-07-25 14:59:11 +0200

    Add a method to populate the Person model with dummy data

--- Person.cs
+++ Person.cs
@@ -6,10 +6,28 @@ namespace ExtensionMethods
 {
   public class Person
   {
+    public Person(string firstName, string lastName, int age, double annualIncome)
+    {
+      FirstName = firstName;
+      LastName = lastName;
+      Age = age;
+      AnnualIncome = annualIncome;
+    }
+
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string FullName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
     public int Age { get; set; }
     public double AnnualIncome { get; set; }
+
+    public void Populate()
+    {
+      List<Person> people = new List<Person>();
+
+      people.Add(new Person("John", "Wick", 49, 26000));
+      people.Add(new Person("James", "Gandolfini", 57, 490000));
+      people.Add(new Person("John", "Gotti", 68, 890000));
+      people.Add(new Person("James", "Last", 77, 645000));
+    }
   }
 }
