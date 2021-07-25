07228c90c71c4bcb5da7d93f1fdc1dab5d488e8b
R3ap4r <R3ap4r@telenet.be>
2021-07-25 16:43:56 +0200

    Moved extensions to their own class for code readability

---
+++ PersonExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+  // Holds the methods for the Person class we're going to extend
+  public static class PersonExtensionMethods
+  {
+    /// <summary>
+    /// Checks if the annual income is at least that of the supplied limit
+    /// </summary>
+    /// <param name="person"></param>
+    /// <param name="limit"></param>
+    /// <returns></returns>
+    public static bool HasMoreThan(this Person person, double limit)
+    {
+      return person.AnnualIncome >= limit;
+    }
+
+    /// <summary>
+    /// Checks if the name provided is shared by anyone else
+    /// </summary>
+    /// <param name="person"></param>
+    /// <param name="toCheckFor"></param>
+    /// <returns></returns>
+    public static bool IsCommon(this Person person, string toCheckFor)
+    {
+      return string.Equals(person.FirstName, toCheckFor, StringComparison.CurrentCultureIgnoreCase);
+    }
+  }
+}
